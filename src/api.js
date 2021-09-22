c4a65358c7848767f25854777be54c30d9323b6f
Dennis Peters <dennis95peters@gmail.com>
2021-09-22 11:02:12 +0300

    Set up the API to pull the data from rawg.io

---
+++ src/api.js
@@ -0,0 +1,45 @@
+//KEY - 0fea486d237a413eb5d8d7960ac0684b
+//https://api.rawg.io/api/games?key=0fea486d237a413eb5d8d7960ac0684b,
+
+
+//Base URL
+
+const base_url = "https://api.rawg.io/api/";
+
+
+//Getting the date month, day, year
+const getCurrentMonth = () => {
+    const month = new Date().getMonth() + 1;
+    if (month < 10) {
+        return `0${month}`
+    }
+    else {
+        return month;
+    }
+}
+
+const getCurrentDay = () => {
+    const day = new Date().getDate();
+    if (day < 10) {
+        return `0${day}`
+    }
+    else {
+        return day;
+    }
+}
+
+//Current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+
+//Popular Games
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10&key=0fea486d237a413eb5d8d7960ac0684b`;
+
+const popularGamesURL = () => `${base_url}${popular_games}`;
+
+console.log(popularGamesURL());

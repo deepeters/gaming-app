e4abddce20b281492a0eed521f47c85924ccd8db
Dennis Peters <dennis95peters@gmail.com>
2021-09-26 14:55:31 +0300

    Enable toggling between the home page and the detail card 2

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 //Styling and Animation
 import styled from "styled-components";
@@ -9,13 +10,24 @@ import { useSelector } from "react-redux";
 
 const GameDetail = () => {
 
+    const history = useHistory()
+
+    //Exit Detail
+    const exitDetailHandler = (e) => {
+        const element = e.target;
+        if (element.classList.contains('shadow')) {
+            document.body.style.overflow = 'auto';
+            history.push('/');
+        }
+    }
+
     //Data
     const {screen, game, isLoading} = useSelector ((state) => state.detail)
 
     return (
         <>
             {!isLoading && (
-                <CardShadow>
+                <CardShadow className = "shadow" onClick = {exitDetailHandler}>
                     <Detail>
                         <Stats>
                             <div className="rating">
